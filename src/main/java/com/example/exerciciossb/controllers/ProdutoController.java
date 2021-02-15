6f6a0d32d1c2e0ea930bd466757fc79263ecf091
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-02-15 16:41:34 -0300

    added the alterarProduto method in the  ProdutoController class to update a product

--- src/main/java/com/example/exerciciossb/controllers/ProdutoController.java
+++ src/main/java/com/example/exerciciossb/controllers/ProdutoController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,5 +39,10 @@ public class ProdutoController {
         return produtoRepository.findById(id);
     }
 
+    @PutMapping
+    public Produto alterarProduto(@Valid Produto produto){
+        produtoRepository.save(produto);
+        return produto;
+    }
 
 }
