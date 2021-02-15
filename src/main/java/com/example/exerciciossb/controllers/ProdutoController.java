7c981a16809242972d0ffcaa1c16a3af7526ad1a
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-02-15 16:30:21 -0300

    alter  ProdutoController class adding methods for consulting all products and by id

--- src/main/java/com/example/exerciciossb/controllers/ProdutoController.java
+++ src/main/java/com/example/exerciciossb/controllers/ProdutoController.java
@@ -1,11 +1,15 @@
 package com.example.exerciciossb.controllers;
 
+import java.util.Optional;
+
 import javax.validation.Valid;
 
 import com.example.exerciciossb.model.entities.Produto;
 import com.example.exerciciossb.model.repositories.ProdutoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,4 +27,16 @@ public class ProdutoController {
         produtoRepository.save(produto);
         return produto;
     }
+
+    @GetMapping
+    public Iterable<Produto> obterProdutos() {
+        return produtoRepository.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<Produto> obterProdutoPorId(@PathVariable int id) {
+        return produtoRepository.findById(id);
+    }
+
+
 }
